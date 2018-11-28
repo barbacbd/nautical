8a40472e586b97d1d0ba0d8a03f895dc957e0070
brent.barbachem <brent.barbachem@sisinc.org>
2018-11-28 10:01:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# SurfForecastScraper
+
+A python based web scraper to grab the surf forecast from [Swellinfo](https://www.swellinfo.com).
+The intention is to move this over to Amazon's Alexa inside of a skill. I would like 
+to be able to ask Alexa [in my house] what the surf forecast for the area is, so that 
+I can make a decision before I try to go surfing. 
+
+This would be easy enough to just program my current location into the Alexa skill, and
+query only the information from that page. I decided that I wanted to expand upon the 
+specific use case, and I would like to create a generic Alexa skill to let me query
+multiple surf locations for their forecast. 
+
+> **Disclaimer** I do NOT pretend to be the creator of the surf forecasts. All rights of any surf forecast belong to [Swellinfo](https://www.swellinfo.com) and their team of forecasters.
---
+++ surf.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import urllib2
+import html2text
+from urlparse import urljoin
+from BeautifulSoup import BeautifulSoup
+import re
+
+'''
+Get the source information for the url and place the information into
+a BeautifulSoup object
+@param url_name - string representation of the url
+@return - BeautifulSoup object containing the source for the url
+'''
+def get_url_source(url_name):
+    # open the url and read the source as text
+    open_url = urllib2.urlopen(url_name)
+
+    # send the read in information to be parsed for relative paths
+    soup = BeautifulSoup(open_url.read())
+
+    return soup
+
+'''
+@param url_name - string representation of the url
+@param soup - BeautifulSoup Object
+@return - list of relative path names converted to no longer be relative
+by adding the name of the url_page they came from.
+'''
+def find_relative_paths(url_name, soup):
+    relative_paths = []
+
+    for url in soup.findAll('a'):
+        path = urljoin(url_name, url.get('href'))
+
+        # only keep the paths that are actually a part of this website
+        if path.startswith(url_name) :
+            relative_paths.append(urljoin(url_name, url.get('href')))
+
+    return relative_paths
+
+'''
+@param relative_paths - list of relative paths from our original source page
+@return - if exists the path to the forecasts page, otherwise return an empty string
+'''
+def find_forecast_url(relative_paths):
+    forecasts = []
+    # grab the forecasts path
+    for url in relative_paths:
+        if "forecast" in url and url not in forecasts:
+            forecasts.append(url)
+
+    forecast_url = ""
+    if len(forecasts) >= 1:
+        forecast_url = forecasts[0]
+
+    return forecast_url
+
+'''
+@param url_name - string representation of the url
+@param surf_loc - location that we wish to append to the url 
+@return - url_name/surf_loc
+'''
+def add_surf_location(url_name, surf_loc):
+
+    url_ends = url_name.ends_with('/')
+    loc_starts = surf_loc.starts_with('/')
+
+    if url_ends and loc_starts:
+        return url_name[:-1] + surf_loc
+    elif (url_ends and not loc_starts) or (not url_ends and loc_starts):
+        return url_name + surf_loc
+    else:
+        return url_name + "/" + surf_loc
+
+'''
+Main Point of execution. Read the url and grab all information. We should slowly work
+our way down to the forecasts for each individual location.
+'''
+def main():
+    # starting url 
+    surf_url = 'https://www.swellinfo.com'
+
+    soup = get_url_source(surf_url)
+    relative_paths = find_relative_paths(surf_url, soup)
+    forecast_url = find_forecast_url(relative_paths)
+
+    # could not find the forecast page, so let's exit
+    if not forecast_url:
+        sys.exit()
+
+    soup = get_url_source(forecast_url)
+    
+    print(soup.prettify())
+    
+
+
+if __name__ == "__main__":
+    main()
