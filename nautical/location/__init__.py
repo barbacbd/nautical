298186fbd48e18a3a496594e8d47360ccc7da0d8
brent.barbachem <brent.barbachem@sisinc.org>
2019-05-27 22:50:01 -0400

    Added functions to determine if a point is in a set of larger points.

--- nautical/location/__init__.py
+++ nautical/location/__init__.py
@@ -1,4 +1,4 @@
-from math import sin, cos, sqrt, radians, atan2
+from math import sin, cos, sqrt, radians, atan2, inf
 
 
 EARTH_RADIUS_METERS = 6372800
@@ -21,6 +21,13 @@ class Point:
         self.set_latitude(lat)
         self.set_longitude(lon)
         self.set_altitude(alt)
+
+    def __str__(self) -> str:
+        """
+        Python version of the to string function. Turn this object into a string
+        :return: string representation of this object
+        """
+        return "[{}, {}, {}]".format(self.lat, self.lon, self.alt)
 
     def set_latitude(self, lat) -> None:
         """
@@ -135,3 +142,46 @@ class Point:
         :return: true if it is in the distance
         """
         return self.get_distance(lat, lon) <= distance
+
+
+def area_converter(area: [Point]) -> [Point]:
+    """
+    Pass in a list of points, determine the maximum and minimum latitude and longitude
+    values, create a square (4 points) from the list.
+    :param area: original list of points
+    :return: list of Points
+    """
+    max_lat = -inf
+    min_lat = inf
+    max_lon = -inf
+    min_lon = inf
+
+    for point in area:
+        if isinstance(point, Point):
+            max_lat = point.lat if point.lat > max_lat else max_lat
+            min_lat = point.lat if point.lat < min_lat else min_lat
+            max_lon = point.lon if point.lon > max_lon else max_lon
+            min_lon = point.lon if point.lon < min_lon else min_lon
+
+    return [Point(min_lat, min_lon), Point(max_lat, max_lon)]
+
+
+def in_area(p: Point, area: [Point]) -> bool:
+    """
+    Determine if point p is in the area. NOTE: the user should pass the original
+    list of points through the area_converter. This will provide an approximate area.
+    :param p: Point to determine if it is in the area
+    :param area: 2 point area min, min -> max, max
+    :return: true if it is in the area false otherwise
+    """
+    if len(area) != 2:
+        print("Nautical Package Error: in_area() -> area should be 2 points (min, min) -> (max, max).")
+        return False
+
+    max_lat = max(area[0].lat, area[1].lat)
+    min_lat = min(area[0].lat, area[1].lat)
+
+    max_lon = max(area[0].lon, area[1].lon)
+    min_lon = min(area[0].lon, area[1].lon)
+
+    return max_lat >= p.lat >= min_lat and max_lon >= p.lon >= min_lon
