45f0ef93da7d47c5bd15370d819381914ef51863
Brent Barbachem <brent.barbachem@sisinc.org>
2020-01-17 23:06:22 -0500

    Finally got the description to show up on pypi.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='nautical',
-    version='1.0.1',
+    version='1.0.2',
     license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'bs4',
