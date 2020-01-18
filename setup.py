a624b1c3c7582097bc566aad481307423a102c03
Brent Barbachem <brent.barbachem@sisinc.org>
2020-01-17 22:36:29 -0500

    Removed requirements file as it was a bad reference.

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as file:
-    reqs = file.read().splitlines()
-
 setup(
     name='nautical',
     version='1.0.0',
     license='MIT',
     packages=find_packages(),
+    install_requires=[
+        'bs4',
+        'pykml'
+    ],
     url='https://barbacbd@bitbucket.org/barbacbd/nautical',
     download_url='https://barbacbd@bitbucket.org/barbacbd/nautical/archive/v_100.tar.gz',
     description='The nautical package is able to lookup NOAA buoy data including swell and wave information.',
     author='Brent Barbachem',
     author_email='barbacbd@dukes.jmu.edu',
     include_package_data=True,
-    install_requires=reqs,
     zip_safe=False
 )
