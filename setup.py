74c9e0060fb61bc653905597ec35ce2fc877457c
Brent Barbachem <brent.barbachem@sisinc.org>
2020-05-16 15:25:50 -0400

    Updated the version to push.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='nautical',
-    version='1.2.0',
+    version='2.0.0',
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
