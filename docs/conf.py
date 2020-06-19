12770f9b5bf93b81f3870a3373be7d726e8bfe46
Brent <Barbachem>
2020-06-19 15:02:20 -0400

    OK don't need all of the documents but i put up a readme for generating more information or more readable information for the user.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Brent Barbachem'
 author = 'Brent Barbachem'
 
 # The full version, including alpha/beta/rc tags
-release = '2.2.0'
+release = '2.2.1'
 
 
 # -- General configuration ---------------------------------------------------
