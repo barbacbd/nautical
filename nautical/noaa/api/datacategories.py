e4b322d41ffa2504c5a4e212242311b56ddbeb6a
brent barbachem <barbacbd@dukes.jmu.edu>
2021-12-02 22:09:49 -0500

    Adding base to package.

--- nautical/noaa/api/datacategories.py
+++ nautical/noaa/api/datacategories.py
@@ -5,67 +5,26 @@ from time import time, sleep
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-class Station:
+class Datacategory:
 
-    elevation = None
-    mindate = None
-    maxdate = None
-    latitude = None
     name = None
-    datacoverage = None
     id = None
-    elevationUnit = None
-    longitude = None
 
     def __init__(self, json_obj): 
         for k, v in json_obj.items():
             if hasattr(self, k):
-                print(f"Setting {k} to {v}")
                 setattr(self, k, v)
 
     def __str__(self):
         return json.dumps(vars(self), indent=4)
 
 
-"""
-Additional Parameters
-
-    datasetid [Optional] 
-        Accepts a valid dataset id or a chain of dataset ids separated by ampersands. Stations 
-        returned will be supported by dataset(s) specified
-    locationid [Optional]
-        Accepts a valid location id or a chain of location ids separated by ampersands. Stations 
-        returned will contain data for the location(s) specified
-    datacategoryid [Optional] 
-        Accepts a valid data category id or an array of data category ids. Stations returned will 
-        be associated with the data category(ies) specified
-datatypeid
-
-extent
-
-startdate
-
-enddate
-
-sortfield
-
-
-sortorder
-
-limit
-
-offset
-
-"""
-
-    
-
-def station_lookup(token, limit, offset):
-    endpoint = "https://www.ncdc.noaa.gov/cdo-web/api/v2/stations"
+def datacategory_lookup(token, limit, offset):
+    endpoint = "https://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories"
     headers = {"Token": token}
     return json.loads(json.dumps(requests.get(endpoint, params={'limit': limit, 'offset': offset}, headers=headers).json()))
     
-def fetch_all_available_stations(token):
+def fetch_all_available_datacategories(token):
     """
     Each token can only be used 5 times per second OR 10,000 times per day.
     Find the number of times that are required to grab all data knowing the max
@@ -99,7 +58,7 @@ def fetch_all_available_stations(token):
     query_results = []
     with ThreadPoolExecutor(max_workers=5) as executor:
         start_time = time()
-        futr_dists = {executor.submit(station_lookup, token, lookup_offsets[k], k): k for k in lookup_offsets}
+        futr_dists = {executor.submit(datacategory_lookup, token, lookup_offsets[k], k): k for k in lookup_offsets}
         for futr in as_completed(futr_dists):
             query_results.append(futr.result())
 
@@ -108,29 +67,18 @@ def fetch_all_available_stations(token):
             # break up the execution so that we don't timeout on requests
             sleep(1.0-run_time)
 
-    print(len(query_results))
-
-
-def fetch_specific_station(token, station_id):
+            
+def fetch_specific_datacategory(token, datacategory_id):
     """
     :param token:
-    :param station_id:
+    :param datacategory_id:
     """
     pass
 
 
-def fetch_stations_by_location(token, location_id):
-    """
-    :param token:
-    :param location_id:
-    """
-    pass
-
-
-def fetch_stations_by_data_types(token, data_types):
+def fetch_datacategories_by_data_types(token, data_types):
     """
     :param token: 
     :param data_types: list of tuples(str, str)
     """
-
     pass
