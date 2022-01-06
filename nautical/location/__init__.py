ded04358a6da371d8dd66dbe9f6c731312f6e1a3
Brent Barbachem <barbacbd@dukes.jmu.edu>
2022-01-06 15:04:48 -0500

    Updating the deploy script as there is an issue with `build`. Downgrading to the last confirmed working version with this project (0.5.0).

--- nautical/location/__init__.py
+++ nautical/location/__init__.py
@@ -2,3 +2,9 @@
 The module contains data to create and manage 3D points. The points can be generated from
 the data contained inside of NOAA's kml and html data.
 """
+from .point import Point
+
+
+__all__ = [
+    "Point"
+]
