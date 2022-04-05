b01eede5f472d50081256407e6588cf045565ef7
Brent Barbachem <bbarbach@redhat.com>
2022-04-04 21:53:01 -0400

    Updating the way the actions are used.

--- tests/test_ncei.py
+++ tests/test_ncei.py
@@ -1,6 +1,16 @@
 import pytest
 from nautical.noaa.ncei import *
 from uuid import uuid4
+from os import environ
+
+
+def _find_test_token() -> str:
+    """
+    In the case of parallelizing unit tests, we will allow the user to
+    set the token via an environment variable `NCEI_TEST_TOKEN`
+    """
+    return environ.get("NCEI_TEST_TOKEN", get_default_token())
+
 
 def test_query_good_token():
     """
@@ -8,7 +18,7 @@ def test_query_good_token():
     Token is good
     Query for a smaller one of types.
     """
-    results = query_all(get_default_token(), obj_type=DataType)
+    results = query_all(_find_test_token(), obj_type=DataType)
     assert len(results) > 0
 
 def test_query_bad_token():
@@ -24,7 +34,7 @@ def test_query_obj_type_bad_base():
     Query All:
     Send in object type of base
     """
-    results = query_all(get_default_token())
+    results = query_all(_find_test_token())
     assert len(results) == 0
 
 def test_query_obj_type_bad_type():
@@ -37,7 +47,7 @@ def test_query_obj_type_bad_type():
             self.data = None
 
     with pytest.raises(AttributeError):
-        results = query_all(get_default_token(), obj_type=ExampleTest)
+        results = query_all(_find_test_token(), obj_type=ExampleTest)
 
 def test_query_bad_endpoint_exists():
     """
@@ -47,7 +57,7 @@ def test_query_bad_endpoint_exists():
     x = DataType.endpoint
     DataType.endpoint = NCEIBase.endpoint + "bad_endpoint"
 
-    results = query_all(get_default_token(), obj_type=DataType)
+    results = query_all(_find_test_token(), obj_type=DataType)
     DataType.endpoint = x
     assert len(results) == 0
 
@@ -59,36 +69,36 @@ def test_query_bad_no_endpoint():
     x = DataType.endpoint
     DataType.endpoint = None
     with pytest.raises(AttributeError):
-        results = query_all(get_default_token(), obj_type=DataType)
+        results = query_all(_find_test_token(), obj_type=DataType)
     DataType.endpoint = x
 
 def test_query_base_good():
     """
     Find the first result in the query
     """
-    results = query_base(get_default_token(), DataType.endpoint, obj_type=DataType)
+    results = query_base(_find_test_token(), DataType.endpoint, obj_type=DataType)
     assert len(results) == 1
 
 def test_query_base_bad():
     """
     Test that the endpoint is bad 
     """
-    results = query_base(get_default_token(), NCEIBase.endpoint+"bad_data", obj_type=DataType)
+    results = query_base(_find_test_token(), NCEIBase.endpoint+"bad_data", obj_type=DataType)
     assert len(results) == 0
 
 def test_query_limit_more_than_one():
     """ 
     Find the first 1000 results in the datatypes endpoint
     """
-    results = query_base(get_default_token(), DataType.endpoint, obj_type=DataType, limit=1000, offset=1)
+    results = query_base(_find_test_token(), DataType.endpoint, obj_type=DataType, limit=1000, offset=1)
     assert len(results) == 1000
 
 def test_query_offset_different():
     """
     Get the first and second results but use two different queries and compare the data
     """
-    results_one = query_base(get_default_token(), DataType.endpoint, obj_type=DataType)
-    results_two = query_base(get_default_token(), DataType.endpoint, obj_type=DataType, offset=2)
+    results_one = query_base(_find_test_token(), DataType.endpoint, obj_type=DataType)
+    results_two = query_base(_find_test_token(), DataType.endpoint, obj_type=DataType, offset=2)
 
     assert len(results_one) == len(results_two)
     assert len(results_one) == 1
