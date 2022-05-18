e44e46be0bc2c90bab2fc23afc2dd378f52dd905
Brent Barbachem <bbarbach@redhat.com>
2022-05-17 22:27:42 -0400

    Setup: Altered the setup and requirements
    ** Added a setup.cfg to offload the work for the setup.py
    
    ** Requirements are added to both setup.cfg and requirements.txt
    as the purpose of each targets a different audience. Changes to one
    require changes to the other.
    
    ** Added a requirements.txt for the tests.

--- setup.py
+++ setup.py
@@ -1,37 +1,13 @@
-from setuptools import setup, find_packages
-from os import path
-from json import loads
-from pip.req import parse_requirements
+from setuptools import setup
 
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+if __name__ == '__main__':
+    '''
+    The main configuration information is located in the setup.cfg
+    file. All changes to package requirements will require a change
+    to both the setup.cfg and requirements.txt file.
 
-with open(path.join(this_directory,"about.json"), "r") as about:
-    jd = loads(about.read())
-
-
-with open(path.join(this_directory, "requirements.txt"), "r") as reqs:
-    requirements = reqs.read().split()
-requirements = [x for x in requirements if x]
-
-    
-setup(
-    name=jd["project"],
-    version=jd["version"],
-    license='MIT',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=find_packages(),
-    python_requires='>=3.6, <4',
-    install_requires=requirements,
-    url=jd["url"],
-    download_url="https://github.com/barbacbd/nautical/archive/v_{}.tar.gz".format(jd["version"].replace(".", "")),
-    description='The nautical package is able to lookup NOAA buoy data including swell and wave information.',
-    author=jd["author"],
-    author_email=jd["email"],
-    package_data={'': ['*.yaml']},
-    include_package_data=True,
-    zip_safe=False
-)
+    Changes to test requirements will require a change to setup.cfg
+    and tests/requirements.txt.
+    '''
+    setup()
