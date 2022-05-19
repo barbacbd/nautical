fdb53a005d2d8ac9b355f260f95211324553296e
Brent Barbachem <bbarbach@redhat.com>
2022-05-19 16:13:51 -0400

    point: Updated location and the utility function
    ** Better checks for in geometry
    ** More Tests
    ** Kept static functions, but gave properties to the location.Point class too.

--- nautical/location/point.py
+++ nautical/location/point.py
@@ -1,114 +1,128 @@
+from logging import getLogger
+from haversine import haversine, Unit
+from nautical.units import DistanceUnits
+
+
+log = getLogger()
+
+
 class Point:
 
-    """
-    A 3D point containing latitude, longitude and altitude coordinates
-    """
-
-    __slots__ = ['_latitude', '_longitude', '_altitude']
+    '''A 3D point containing latitude, longitude and altitude coordinates.'''
 
     def __init__(self, lat: float = 0.0, lon: float = 0.0, alt: float = 0.0) -> None:
-        """
-        :param lat: latitude degrees
-        :param lon: Longitude degrees
-        :param alt: Altitude meters
-        """
-        self._latitude = 0.0
-        self._longitude = 0.0
-        self._altitude = 0.0
+        '''The latitude, longitude, and altitude are supplied to
+        the base class as the x, y, z parameters respectively.
+        '''
+        self._latitude = lat
+        self._longitude = lon
+        self._altitude = alt
 
-        # use the protected setters
-        self.latitude = lat
-        self.longitude = lon
-        self.altitude = alt
-
+        # aliases
+        self.x = self.latitude
+        self.y = self.longitude
+        self.z = self.altitude
+        
     @property
     def latitude(self):
-        """Latitude Property (degrees)
+        '''Latitude Property (degrees)
         :return: latitude (degrees)
-        """
+        '''
         return self._latitude
 
     @property
     def longitude(self):
-        """Longitude Property (degrees)
+        '''Longitude Property (degrees)
         :return: longitude (degrees)
-        """
+        '''
         return self._longitude
 
     @property
     def altitude(self):
-        """Altitude Property (meters)
+        '''Altitude Property (meters)
         :return: altitude (meters)
-        """
+        '''
         return self._altitude
 
-    @latitude.setter
-    def latitude(self, latitude):
-        """Setter/Validity check for latitude
-        :param latitude: latitude (degrees) that must fall between -90 and 90
-        """
-        if isinstance(latitude, float):
-            if -90.0 <= latitude <= 90.0:
-                self._latitude = float(latitude)
+    def as_tuple(self):
+        '''Get the values of the object as a simple tuple. The
+        lat and lon are used but not the altitude. The altitude
+        is omitted for use with haverine.
 
-    @longitude.setter
-    def longitude(self, longitude):
-        """Setter/Validity check for longitude
-        :param longitude: longitude (degrees) that must fall between -180 and 80
-        """
-        if isinstance(longitude, float):
-            if -180.0 <= longitude <= 180.0:
-                self._longitude = longitude
-
-    @altitude.setter
-    def altitude(self, altitude):
-        """Setter/Validity check for meters. Negative values are considered
-        depth, but are placed in the same class variable.
-
-        :param altitude: any floating point value is accepted
-        """
-        if isinstance(altitude, float):
-            self._altitude = altitude
-
+        :return: Tuple of lat, lon
+        '''
+        return self.latitude, self.longitude
+    
     def __str__(self) -> str:
-        """
+        '''
         Python version of the to string function. Turn this object into a string
 
         :return: string representation of this object
-        """
-        return "{}, {}, {}".format(self._latitude, self._longitude, self.altitude)
+        '''
+        return "{}, {}, {}".format(self.latitude, self.longitude, self.altitude)
 
-    def __hash__(self):
-        """
-        Each 3D point should be unique and there can only be one 3D point in the world that matches lat, lon, alt
+    def distance(self, other, units = DistanceUnits.METERS):
+        '''Get the distance using the Haversine function. The function will
+        determine the distance between this instance and another `Point`.
+        
+        :param other: The other `Point`
+        :param units: Units used for measurement
+        :return: Distance between the points in units specified
+        '''
+        if not isinstance(units, DistanceUnits):
+            raise TypeError("DistanceUnits not found: {}".format(str(type(units))))
+        elif not isinstance(other, Point):
+            raise TypeError("Distance should be calculated between two points")
+        
+        if units == DistanceUnits.CENTIMETERS:
+            log.error("haversine does not support units: {}".format(units.name))
+            return
+        
+        hav_units = getattr(Unit, str(units.name), Unit.METERS)
+        log.debug("haversine executed with units: {}".format(hav_units))
+        
+        return haversine(self.as_tuple(), other.as_tuple(), hav_units)
 
-        :return: hash of the latitude * longitude * altitude
-        """
-        _h = [x for x in (self.latitude, self.longitude, self.altitude) if x != 0.0]
+    def in_range(self, other, distance, units = DistanceUnits.METERS):
+        '''Deteremine if the points are within a specific distance of eachother.
+        
+        :param other: The other `Point`
+        :param distance: Max distance between the points
+        :param units: Units used for measurement
+        :return: True when points are within the specified distance
+        '''
+        try:
+            return self.distance(other, units) <= distance
+        except TypeError as e:
+            log.error(e)
+            return False
+        
+    @staticmethod
+    def parse(data):
+        '''Parse the string containing lon, lat, alt [optional] values respectively.
 
-        _prod = 1.0
-        for _i in _h:
-            _prod *= _i
-
-        return hash(str(_prod))
-
-    def parse(self, data: str) -> None:
-        """Parse the string containing lat, lon, alt [optional] 
-
-        .. note:: Comma separated data is parsed as lon, lat, alt [optional]
-
-        :param data: A string containing (whitespace ignored and not a delimiter) lat, lon, altitude.
-        """
-        if data:
-            """ Remove all whitespace and lower case the value"""
+        :param data: A string containing lon, lat, altitude.
+        :return: instance of Point on success, None on failure
+        '''
+        try:
             data = data.lower()
-            data = "".join(data.split())
             split_data = data.split(",")
+            
+            # flip positions to lat, lon
+            args = [float(split_data[1].strip()), float(split_data[0].strip())]
+            if len(split_data) > 2:
+                # add altitude if exists
+                args.append(float(split_data[2].strip()))
+                
+            return Point(*args)
+                    
+        except (IndexError, TypeError) as e:
+            log.error(e)
+            raise # save stack information
 
-            try:
-                self.longitude = float(split_data[0])  # should ALWAYS exist
-                self.latitude = float(split_data[1])   # should ALWAYS exist
-                if len(split_data) > 2:
-                    self.altitude = float(split_data[2])   # may or may not exist
-            except (IndexError, TypeError) as e:
-                raise # save stack information
+    @staticmethod
+    def parse_noaa_kml_format(data):
+        '''Parse the string containing lon, lat, alt [optional] values respectively.
+        See `Point.parse` for implementation and notes.
+        '''
+        return Point.parse(data)
