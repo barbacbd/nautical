95127e90f23f0c8ae5465f64a742d427b0e40125
Brent Barbachem <bbarbach@redhat.com>
2022-07-15 11:26:27 -0400

    Removed extra reqs and updated conf

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../nautical'))
+sys.path.insert(0, os.path.abspath('../'))
 import subprocess
 from datetime import datetime
 
@@ -86,7 +86,7 @@ exclude_patterns = ['venv', '__pycache__']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
