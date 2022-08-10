be54d64289bb5a207e124b14012f4cdd43a0cc17
Brent Barbachem <bbarbach@redhat.com>
2022-08-09 21:44:15 -0400

    Added few more tests for parameters.

--- pkg/noaa/ncei/ncei_test.go
+++ pkg/noaa/ncei/ncei_test.go
@@ -1,11 +1,16 @@
 package ncei
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestInArea(t *testing.T) {
+var (
+	testEndpoint = "https://test.endpoint.com"
+)
+
+func TestOffset(t *testing.T) {
 	tests := []struct {
 		name         string
 		count        int
@@ -61,6 +66,57 @@ func TestInArea(t *testing.T) {
 					assert.Equal(t, offsets[maxKey], tc.expectedLeft)
 				}
 			}
+		})
+	}
+}
+
+func TestAddToEndpoint(t *testing.T) {
+	tests := []struct {
+		name          string
+		allowedParams []string
+		userParams    []Parameter
+		expected      string
+	}{{
+		name:     "Base Endpoint No Parameters",
+		expected: testEndpoint,
+	}, {
+		name: "No Allowed Params",
+		userParams: []Parameter{
+			{Name: "test1", Value: "test1"},
+			{Name: "test2", Value: "test2"},
+		},
+		expected: testEndpoint,
+	}, {
+		name:          "Single Allowed Params",
+		allowedParams: []string{"test1"},
+		userParams: []Parameter{
+			{Name: "test1", Value: "test1"},
+			{Name: "test2", Value: "test2"},
+		},
+		expected: fmt.Sprintf("%s?test1=test1", testEndpoint),
+	}, {
+		name:          "Multiple Allowed Params",
+		allowedParams: []string{"test1", "test3"},
+		userParams: []Parameter{
+			{Name: "test1", Value: "test1"},
+			{Name: "test2", Value: "test2"},
+			{Name: "test3", Value: "test3"},
+		},
+		expected: fmt.Sprintf("%s?test1=test1&test3=test3", testEndpoint),
+	}, {
+		name:          "Mulitple Allowed Params Find One",
+		allowedParams: []string{"test1", "test3"},
+		userParams: []Parameter{
+			{Name: "test1", Value: "test1"},
+			{Name: "test2", Value: "test2"},
+		},
+		expected: fmt.Sprintf("%s?test1=test1", testEndpoint),
+	}}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result := addParamsToEndpoint(testEndpoint, tc.allowedParams, tc.userParams)
+			assert.Equal(t, tc.expected, result)
 		})
 	}
 }
